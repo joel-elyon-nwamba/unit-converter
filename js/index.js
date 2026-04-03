8ef7432501cf401633c7da55c89dda096d951999
joel-nwamba <joel.elyon.nwamba@gmail.com>
2026-04-03 16:00:52 -0400

    Adding DOM elements, and initiating variable for meters to feet, kilos to pound and litre to gallon

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// Input value to insert number
+const inputValue = document.getElementById("input-value");
+// Button that will convert numbers 
+const convertBtn = document.getElementById("convert-btn");
+
+const lengthResult = document.getElementById("length-result");
+const volumeResult = document.getElementById("volume-result");
+const massRsult = document.getElementById("mass-result");
+
+const metersToFeet = 3.281;
+const litresToGallon = 0.264;
+const kiloToPound = 2.204;
+
