ffbbe6c56f152e445191360ca13dc36c7121effd
joel-nwamba <joel.elyon.nwamba@gmail.com>
2026-04-03 20:06:03 -0400

    Able to convert unit to feet, gallons, and pounds, now need to convert to the opposite

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ const convertBtn = document.getElementById("convert-btn");
 
 const lengthResult = document.getElementById("length-result");
 const volumeResult = document.getElementById("volume-result");
-const massRsult = document.getElementById("mass-result");
+const massResult = document.getElementById("mass-result");
 
 const metersToFeet = 3.281;
 const litresToGallon = 0.264;
@@ -14,7 +14,23 @@ const kiloToPound = 2.204;
 // Function meters to feet 
 convertBtn.addEventListener("click", (e) => {
         e.preventDefault();
-    let baseValue = inputValue.value 
-
-    lengthResult.textContent = `${baseValue} meters = ${baseValue * metersToFeet} feet`;
+    metersFeet();
+    litresGallon();
+    kilosPounds();
 });
+
+function metersFeet() {
+     let baseValue = inputValue.value 
+    // const finalResult = 
+    lengthResult.textContent = `${baseValue} meters = ${baseValue * metersToFeet} feet`;
+}
+
+function litresGallon() {
+    let baseValue = inputValue.value 
+    volumeResult.textContent = `${baseValue} litres = ${baseValue * litresToGallon} gallons`
+}
+
+function kilosPounds() {
+    let baseValue = inputValue.value 
+    massResult.textContent = `${baseValue} kilograms = ${baseValue * kiloToPound} pounds`
+}
